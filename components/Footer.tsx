33aea181405a1cca539fd394ae868e072154720f
Turbobot <turbobot@vercel.com>
2023-09-26 14:22:20 +0900

    feat(create-turbo): apply official-starter transform

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import classNames from "classnames/bind";
-import style from "./Footer.module.scss";
-const cx = classNames.bind(style);
-
-export default function Footer() {
-  return <div className={cx("container")}>@winterlood</div>;
-}
