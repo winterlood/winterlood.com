483a7b8efb8a8fbf3c56068702cc7039ab03fa16
winterlood <king199777@gmail.com>
2023-10-02 19:44:40 +0900

    ✨ sitemap 추가

---
+++ apps/web/app/sitemap.ts
@@ -0,0 +1,28 @@
+import { fetchPages } from "util/fetch-pages";
+
+const BASE_URL = process.env.BASE_URL;
+
+export default async function sitemap() {
+  const posts = (await fetchPages("POST")).map((page) => ({
+    url: `${BASE_URL}/post/${page.id}`,
+    lastModified: new Date(page.createTime).toISOString(),
+  }));
+
+  const qnas = (await fetchPages("QNA")).map((page) => ({
+    url: `${BASE_URL}/qna/${page.id}`,
+    lastModified: new Date(page.createTime).toISOString(),
+  }));
+
+  const routes = ["", "/post", "/qna", "/work"].map((route) => ({
+    url: `${BASE_URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  const res = [...routes, ...posts, ...qnas].sort(
+    (a, b) =>
+      new Date(b.lastModified).getTime() -
+      new Date(a.lastModified).getTime()
+  );
+
+  return res;
+}
