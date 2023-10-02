eeb0bac3fc5f7cdf560b14f590cf393d31212fce
winterlood <king199777@gmail.com>
2023-10-02 13:56:28 +0900

    ✨ / 렌더러 추가

--- apps/web/app/(root)/page.tsx
+++ apps/web/app/(root)/page.tsx
@@ -1,11 +1,16 @@
 import classNames from "classnames/bind";
 import style from "./page.module.scss";
 import { fetchPage } from "util/fetch-page";
+import NotionRenderer from "@/components/NotionRenderer";
 
 const cx = classNames.bind(style);
 
 export default async function Page() {
   const { info, recordMap } = await fetchPage("ABOUT");
 
-  return <main className={cx("container")}>{info.title}</main>;
+  return (
+    <main className={cx("container")}>
+      <NotionRenderer recordMap={recordMap} />
+    </main>
+  );
 }
